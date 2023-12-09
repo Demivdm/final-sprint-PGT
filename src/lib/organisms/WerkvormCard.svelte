7f60a78bd08e7d19928e9266c41d32bd1a6e7d1b
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-12-09 16:40:14 +0100

    💫 Added werkvorm card animation

--- src/lib/organisms/WerkvormCard.svelte
+++ src/lib/organisms/WerkvormCard.svelte
@@ -4,21 +4,18 @@
 </script>
 
 <article>
+	<ul>
+		{#each werkvorm.tags as tag}
+			<li style="border-color: {tag.kleur.hex};">{tag.titel}</li>
+		{/each}
+	</ul>
 	<img src={werkvorm.thumbnail.url} alt="" />
 	<div>
         <div class="werkvorm-content">
             <h2>{werkvorm.title}</h2>
             <p>{werkvorm.korteBeschrijving}</p>
-
         </div>
-        <div>
-            <a href={werkvorm.link}><img src="images/icons/arrow-right.svg" alt="" />Bekijk werkvorm</a>
-            <ul>
-                {#each werkvorm.tags as tag}
-                    <li style="border-color: {tag.kleur.hex};">{tag.titel}</li>
-                {/each}
-            </ul>
-        </div>
+        <a href={werkvorm.link}><img src="images/icons/arrow-right.svg" alt="" />Bekijk werkvorm</a>
 	</div>
 </article>
 
@@ -34,19 +31,34 @@
 		background: var(--color-hva-navy);
         box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.25);
         max-width: 20rem;
+		padding: 0.8rem;
 	}
 
 	article > img {
-		height: 8rem;
+		height: 10rem;
 		width: 100%;
 		object-fit: cover;
 		object-position: center;
+		transition: all ease-in-out 0.2s;
+	}
+
+	article:hover > img {
+		height: 7.5rem;
+		margin-top: 2.5rem;
+	}
+
+	article:hover > ul {
+		z-index: 0;
+		/* opacity: 1; */
+	}
+
+	article:hover a {
+		transform: translateX(0.8rem);
 	}
 
 	article > div {
 		display: flex;
 		flex-direction: column;
-		padding: 0 1rem 1rem;
         height: 100%;
         justify-content: space-between;
 	}
@@ -69,10 +81,15 @@
 		align-items: center;
 		column-gap: 0.5rem;
 		margin-left: 0.5rem;
-        margin-bottom: 1.2rem;
+        margin-bottom: 0.8rem;
 		text-decoration: underline;
 		font-weight: 600;
 		font-size: 1rem;
+		transition: all ease-in-out 0.2s;
+	}
+
+	a:focus {
+		outline: solid 2px var(--color-hva-pink);
 	}
 
 	a > img {
@@ -87,6 +104,10 @@
         list-style: none;
         column-gap: 0.5rem;
         text-transform: capitalize;
+		position: absolute;
+		z-index: -1;
+		/* opacity: 0; */
+		transition: all ease-out 0.2s;
     }
 
     li {
