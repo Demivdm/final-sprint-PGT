5ebc1e6bf3cea9b78c9d2b1d2e15976f01c6fc07
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-12-18 23:41:58 +0100

    🐛 fixed menu icon

--- src/lib/organisms/nav.svelte
+++ src/lib/organisms/nav.svelte
@@ -32,8 +32,8 @@
 			<figure>
 				<div>
 					<svg
-						width=""
-						height=""
+						width="24"
+						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
 						xmlns="http://www.w3.org/2000/svg"
@@ -56,13 +56,13 @@
 						</g>
 						<defs>
 							<clipPath id="clip0_937_7">
-								<rect width="" height="" fill="white" />
+								<rect width="24" height="24" fill="white" />
 							</clipPath>
 						</defs>
 					</svg>
 					<svg
-						width=""
-						height=""
+						width="24"
+						height="24"
 						viewBox="0 0 24 24"
 						fill="none"
 						xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +85,7 @@
 						</g>
 						<defs>
 							<clipPath id="clip0_937_18">
-								<rect width="" height="" fill="white" />
+								<rect width="24" height="24" fill="white" />
 							</clipPath>
 						</defs>
 					</svg>
@@ -310,11 +310,6 @@
 	* {
 		color: white;
 		margin: 0;
-	}
-
-	svg {
-		width: 1.5rem;
-    	height: 1.5rem;
 	}
 
 	nav {
