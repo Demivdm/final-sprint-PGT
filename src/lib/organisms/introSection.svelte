64d94c6f1ab398e7aeb53fa0e9621a1cde0d35c4
Stefan Espant <stefan.van.der.kort@hva.nl>
2024-01-18 13:07:50 +0100

    💄 added intro section with necessary content

---
+++ src/lib/organisms/introSection.svelte
@@ -0,0 +1,10 @@
+<section>
+    <h1>Welkom op het platform Werkvormen Grote Thema's</h1>
+    <p>Wil jij met jouw studenten werken aan ‘moeilijke’ maatschappelijke vraagstukken?  Vind hier een werkvorm die helpt om grote thema’s bespreekbaar en behapbaar te maken.</p>
+</section>
+
+<style>
+    section {
+        padding: var(--unit-default) var(--unit-default) 5rem;
+    }
+</style>
