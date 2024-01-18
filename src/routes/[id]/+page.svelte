2af20ebe802dd6a36cdcbde552c49d4d68fff5f3
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2024-01-18 21:35:33 +0100

    🐛 Fixed thumbnail showing rotated on detailpage

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -28,6 +28,7 @@
 		<!-- svelte-ignore a11y-media-has-caption -->
 		{#if workform.video == null}
 			<img
+				class="thumbnail"
 				src={'https://platform-big-themes.directus.app/assets/' + workform.thumbnail.id}
 				alt="Thumbnail"
 			/>
@@ -216,6 +217,11 @@
 		max-width: 28rem;
 	}
 
+	.thumbnail {
+		transform: unset;
+		height: unset;
+	}
+
 	/* Layout content werkvorm */
 	article.content {
 		display: grid;
@@ -279,7 +285,7 @@
         section {
             display: grid;
             grid-template-areas:
-            "werkvorm-title uploadbutton" 
+            "werkvorm-title uploadbutton"
             "videoplayer werkvorm-content";
             grid-auto-columns: 3fr 2fr;
             margin: 0 auto;
@@ -288,9 +294,9 @@
 
 		article.content {
 			grid-template:
-                "a" 
-                "b" 
-                "c" 
+                "a"
+                "b"
+                "c"
                 "d"
             ;
             grid-area: werkvorm-content;
